1f0155b11db3526e1fe648312bd01865d4734a6f
martyloo <63059963+martyloo@users.noreply.github.com>
2026-08-26 23:36:56 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.oddscompare.app"
-    compileSdk = 37
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.oddscompare.app"
         minSdk = 24
-        targetSdk = 37
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
     }
